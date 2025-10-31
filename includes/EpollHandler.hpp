a41b08a38b6491327c016645f737d73aa1c0bcba
jpedrobacker <jpedrobacker@outlook.com>
2025-10-31 13:49:01 -0300

    Add: Epoll handler setu up

--- includes/EpollHandler.hpp
+++ includes/EpollHandler.hpp
@@ -6,15 +6,31 @@
 /*   By: jbergfel <jbergfel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 18:37:48 by jbergfel          #+#    #+#             */
-/*   Updated: 2025/10/31 07:48:42 by jbergfel         ###   ########.fr       */
+/*   Updated: 2025/10/31 13:46:55 by jbergfel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+# include "Webserv.hpp"
 
 class EpollHandler
 {
 	private:
+		int _socketFd;
+		uint32_t _activeEvents;
 
 	public:
+		~EpollHandler();
+		EpollHandler(uint32_t activeEvents);
+		EpollHandler(int fd, uint32_t activeEvents);
+
+		int EpollEventHandler(struct epoll_event &event);
+		void EpollInHandler();
+		void EpollOutHandler();
+
+		//Getter e Setter
+
+		void setSocketFd(int socketFd);
+		int getSocketFd();
+		uint32_t getActiveEvents();
 };
