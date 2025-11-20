c27b257648065ab841275896b4935d177df7e679
Luara Raggio <luararaggio@hotmail.com>
2025-11-19 22:23:15 -0300

    feat(feat/12): add file dinamic log handlers implementations to webserv Logger, so we can change between handlers and format outputs

---
+++ includes/CompositeLogHandler.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CompositeLogHandler.hpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lraggio <lraggio@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/19 22:14:00 by lraggio           #+#    #+#             */
+/*   Updated: 2025/11/19 22:15:49 by lraggio          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COMPOSITELOGHANDLER_HPP
+# define COMPOSITELOGHANDLER_HPP
+
+
+#include "Logger.hpp"
+#include <vector>
+
+class CompositeLogHandler : public LogHandler
+{
+private:
+	std::vector<LogHandler*> _handlers;
+
+public:
+	CompositeLogHandler();
+	virtual ~CompositeLogHandler();
+
+	void addHandler(LogHandler *handler);
+
+	virtual void handleDebug(t_event event);
+	virtual void handleInfo(t_event event);
+	virtual void handleWarning(t_event event);
+	virtual void handleError(t_event event);
+};
+
+#endif
