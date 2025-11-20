d05585c72cb70edfd65ebe5b7b4d05f00ef44f38
Luara Raggio <luararaggio@hotmail.com>
2025-11-19 22:22:01 -0300

    feat(File Log Handler): add file log handler implementation to webserv Logger

---
+++ includes/FileLogHandler.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FileLogHandler.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lraggio <lraggio@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/19 22:16:07 by lraggio           #+#    #+#             */
+/*   Updated: 2025/11/19 22:16:14 by lraggio          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILELOGHANDLER_HPP
+#define FILELOGHANDLER_HPP
+
+#include "Logger.hpp"
+#include <fstream>
+
+class FileLogHandler : public LogHandler
+{
+private:
+	std::ofstream _file;
+
+public:
+	FileLogHandler(const std::string &filename);
+	virtual ~FileLogHandler();
+
+	virtual void handleDebug(t_event event);
+	virtual void handleInfo(t_event event);
+	virtual void handleWarning(t_event event);
+	virtual void handleError(t_event event);
+};
+
+#endif
